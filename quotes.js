1d4cbb67b83e3121234ca27e9225d24814b4102d
bala-11 <balajikalimuthu482@gmail.com>
2026-09-21 12:24:57 +0530

    new index

--- quotes.js
+++ quotes.js
@@ -1,1 +1,18 @@
-window.QUOTES = [];
+window.QUOTES = [
+  {
+    "id": "dfa94a67-a698-4a53-bc06-6ff600bdab26",
+    "text": "welcome",
+    "book": "welcome",
+    "author": "wel",
+    "color": 1,
+    "createdAt": 1789973633467
+  },
+  {
+    "id": "fa9bd8d7-b06c-4526-a148-69e493ed7300",
+    "text": "hi",
+    "book": "hi",
+    "author": "ii",
+    "color": 3,
+    "createdAt": 1789973428286
+  }
+];
