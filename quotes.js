183b83281abc239c34de898ee164840fa1d68e5c
bala-11 <balajikalimuthu482@gmail.com>
2026-09-21 12:45:34 +0530

    added alchemist quotes

--- quotes.js
+++ quotes.js
@@ -1,18 +1,18 @@
 window.QUOTES = [
   {
-    "id": "dfa94a67-a698-4a53-bc06-6ff600bdab26",
-    "text": "And, when you want something, all the universe conspires in helping you to achieve it",
+    "id": "36e8312e-89ab-48b5-9073-e9f1fd4f1b2e",
+    "text": "\"It's the possibility of having a dream come true that makes life interesting.\"",
+    "book": "The Alchemist",
+    "author": "Paulo coelho",
+    "color": 6,
+    "createdAt": 1789974884019
+  },
+  {
+    "id": "90c39b70-339d-4691-962a-e694b5015820",
+    "text": "\"And, when you want something, all the universe conspires in helping you to achieve it\"",
     "book": "The Alchemist",
     "author": "Paulo coelho",
     "color": 1,
-    "createdAt": 1789973633467
-  },
-  {
-    "id": "fa9bd8d7-b06c-4526-a148-69e493ed7300",
-    "text": "It's the possibility of having a dream come true that makes life interesting.",
-    "book": "The Alchemist",
-    "author": "Paulo coelho",
-    "color": 3,
-    "createdAt": 1789973428286
+    "createdAt": 1789974862704
   }
 ];
