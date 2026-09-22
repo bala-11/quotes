d72478b2d379af7427b6007b32256025a902e97e
bala-11 <balajikalimuthu482@gmail.com>
2026-09-22 16:32:24 +0530

    added robin sharma quote

--- quotes.js
+++ quotes.js
@@ -14,5 +14,13 @@ window.QUOTES = [
     "author": "Paulo Coelho",
     "color": 3,
     "createdAt": 1789973428286
+  },
+  {
+    "id": "fa9bd8d7-b06c-4526-a148-69e493ed7301",
+    "text": "Business is not about sharing money, it is about relationships",
+    "book": "The Master Manual",
+    "author": "Robin Sharma",
+    "color": 4,
+    "createdAt": 1789973428289
   }
 ];
